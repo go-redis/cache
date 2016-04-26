5b60adbd3fcf4a4c7737da8508c02249c22e9dbe
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2016-04-26 13:07:25 +0300

    Fix getting nil value with LRU cache.

--- cache_test.go
+++ cache_test.go
@@ -10,6 +10,7 @@ import (
 	"gopkg.in/vmihailenco/msgpack.v2"
 
 	"gopkg.in/go-redis/cache.v4"
+	"gopkg.in/go-redis/cache.v4/lrucache"
 )
 
 func TestModels(t *testing.T) {
@@ -18,10 +19,62 @@ func TestModels(t *testing.T) {
 }
 
 var _ = Describe("Codec", func() {
+	var ring *redis.Ring
 	var codec *cache.Codec
 
+	testCodec := func() {
+		It("Gets and Sets data", func() {
+			key := "mykey"
+			obj := Object{
+				Str: "mystring",
+				Num: 42,
+			}
+
+			err := codec.Set(&cache.Item{
+				Key:        key,
+				Object:     obj,
+				Expiration: time.Hour,
+			})
+			Expect(err).NotTo(HaveOccurred())
+
+			var wanted Object
+			err = codec.Get(key, &wanted)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(wanted).To(Equal(obj))
+		})
+
+		It("Gets and Sets nil", func() {
+			key := "mykey"
+
+			err := codec.Set(&cache.Item{
+				Key:        key,
+				Expiration: time.Hour,
+			})
+			Expect(err).NotTo(HaveOccurred())
+
+			err = codec.Get(key, nil)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("Deletes key", func() {
+			key := "mykey"
+
+			err := codec.Set(&cache.Item{
+				Key:        key,
+				Expiration: time.Hour,
+			})
+			Expect(err).NotTo(HaveOccurred())
+
+			err = codec.Delete(key)
+			Expect(err).NotTo(HaveOccurred())
+
+			err = codec.Get(key, nil)
+			Expect(err).To(Equal(cache.ErrCacheMiss))
+		})
+	}
+
 	BeforeEach(func() {
-		ring := redis.NewRing(&redis.RingOptions{
+		ring = redis.NewRing(&redis.RingOptions{
 			Addrs: map[string]string{
 				"server1": ":6379",
 				"server2": ":6380",
@@ -31,65 +84,40 @@ var _ = Describe("Codec", func() {
 			ReadTimeout:  time.Second,
 			WriteTimeout: time.Second,
 		})
-
-		codec = &cache.Codec{
-			Redis: ring,
-
-			Marshal: func(v interface{}) ([]byte, error) {
-				return msgpack.Marshal(v)
-			},
-			Unmarshal: func(b []byte, v interface{}) error {
-				return msgpack.Unmarshal(b, v)
-			},
-		}
 	})
 
-	It("Gets and Sets data", func() {
-		key := "mykey"
-		obj := Object{
-			Str: "mystring",
-			Num: 42,
-		}
+	Context("without Cache", func() {
+		BeforeEach(func() {
+			codec = &cache.Codec{
+				Redis: ring,
 
-		err := codec.Set(&cache.Item{
-			Key:        key,
-			Object:     obj,
-			Expiration: time.Hour,
+				Marshal: func(v interface{}) ([]byte, error) {
+					return msgpack.Marshal(v)
+				},
+				Unmarshal: func(b []byte, v interface{}) error {
+					return msgpack.Unmarshal(b, v)
+				},
+			}
 		})
-		Expect(err).NotTo(HaveOccurred())
 
-		var wanted Object
-		err = codec.Get(key, &wanted)
-		Expect(err).NotTo(HaveOccurred())
-		Expect(wanted).To(Equal(obj))
+		testCodec()
 	})
 
-	It("Gets and Sets nil", func() {
-		key := "mykey"
+	Context("with Cache", func() {
+		BeforeEach(func() {
+			codec = &cache.Codec{
+				Redis: ring,
+				Cache: lrucache.New(time.Minute, 1000),
 
-		err := codec.Set(&cache.Item{
-			Key:        key,
-			Expiration: time.Hour,
+				Marshal: func(v interface{}) ([]byte, error) {
+					return msgpack.Marshal(v)
+				},
+				Unmarshal: func(b []byte, v interface{}) error {
+					return msgpack.Unmarshal(b, v)
+				},
+			}
 		})
-		Expect(err).NotTo(HaveOccurred())
 
-		err = codec.Get(key, nil)
-		Expect(err).NotTo(HaveOccurred())
-	})
-
-	It("Deletes key", func() {
-		key := "mykey"
-
-		err := codec.Set(&cache.Item{
-			Key:        key,
-			Expiration: time.Hour,
-		})
-		Expect(err).NotTo(HaveOccurred())
-
-		err = codec.Delete(key)
-		Expect(err).NotTo(HaveOccurred())
-
-		err = codec.Get(key, nil)
-		Expect(err).To(Equal(cache.ErrCacheMiss))
+		testCodec()
 	})
 })
